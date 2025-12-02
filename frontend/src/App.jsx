648f5f41f4372c4b5b5d11f20a5342977f3009b2
NGO-Forum <support@ngoforum.org.kh>
2025-12-02 14:49:26 +0700

    feature: add the structure project frontend folder and project frontend with react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './assets/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' text-emerald-500  text-center'>Welcome to the ReactJs Project with Tailwindcss</h1>
+    </>
+  )
+}
+
+export default App
